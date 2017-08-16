88b8201e5156aee5edd516405455179175bacd84
carlnues@buffalo.edu <carlnues@buffalo.edu>
2017-08-16 12:33:35 -0400

    Rejiggered blocking classifier algo

--- scripts/analytics/findtimeblockedonio.py
+++ scripts/analytics/findtimeblockedonio.py
@@ -25,6 +25,7 @@ end_time = 0.0
 
 with gzip.open(workload,'r') as log:
 	between = 0
+	iomode = False
 	for line in log:
 		if dbType + '_START' in line:
 			start_time = float(line.split()[3].split(':')[0])
@@ -35,6 +36,7 @@ with gzip.open(workload,'r') as log:
 			assert(mypid != 0)
 		if between == 0:
 			continue
+		'''
 		if resetline == True:
 			if line == holdline:
 				resetline = False
@@ -42,6 +44,7 @@ with gzip.open(workload,'r') as log:
 				continue
 			else:
 				continue
+		'''
 		if 'prev_pid='+mypid in line:
 			sced_on_core = 999
 			core = int(line.split(']')[0].split('[')[1])
@@ -51,12 +54,55 @@ with gzip.open(workload,'r') as log:
 		if dbType + '_END' in line:
 			end_time = float(line.split()[3].split(':')[0])
 			break
-		if 'block_rq_insert' in line:
-			insertaddr = line.split()[9] + ' + ' + line.split()[11]
-			
-			if 'withjson' not in line:
-				continue
-			
+
+		# Thread queues I/O
+		if (('block_rq_insert' in line) and ('withjson' in line)):
+			iomode = True
+			continue
+		#end_if
+		# TODO:  trap for double inserting?  (can happen?)
+
+		# Thread blocks:
+		if ((iomode == True) and ('prev_pid='+mypid in line)):
+			timestampstart = float(line.split()[3].split(':')[0])
+			sced_on_core = 999
+			core = int(line.split(']')[0].split('[')[1])
+
+			continue
+		#end_if
+		# TODO:  trap for double blocking
+		# TODO:  need to track core #??
+
+
+		# Thread runs:
+		if ((iomode == True) and ('next_pid='+mypid in line)):
+			core = int(line.split(']')[0].split('[')[1])
+			sced_on_core = core
+
+			timestampend = float(line.split()[3].split(':')[0])
+			total += (timestampend - timestampstart)*1000
+			data[dict_count] = [['IO Event', line],['Waiting time',(timestampend - timestampstart)*1000]]
+			dict_count += 1
+
+			continue
+		#end_if
+		# TODO:  trap for orphan unblocking
+		# TODO:  need to track core #?
+
+		# Thread dequeues I/O
+		if (('block_rq_complete' in line) and ('withjson' in line)):
+			iomode = False
+
+			continue
+		#end_if
+		# TODO:  Need to filter for correct PID
+
+	#end_for
+
+#end_with
+
+
+'''
 			with gzip.open(workload, 'r') as log2:
 				linecount = 0
 				desced = False
@@ -95,6 +141,10 @@ with gzip.open(workload,'r') as log:
 						if insertaddr in line2:
 							if desced == True:
 								iocomplete = True
+
+
+'''
+
 
 assert(mypid != 0)
 
