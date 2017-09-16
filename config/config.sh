8a614018075b0c6d57dc1b676a5027647645f636
carlnues@buffalo.edu <carlnues@buffalo.edu>
2017-09-16 16:34:19 -0400

    Changed phone CPU config to to perf governor for all cores. Disabled SELinux at shell (permission error with AOSP image). Removed config directory (redundant).

--- config/config.sh
+++
@@ -1,4 +0,0 @@
-echo 'Configuring...'
-rm ../apps/Benchmark_withJson/local.properties
-cp local.properties ../apps/Benchmark_withJson/
-echo 'Done.'
--- files/phone/benchmark_50.sh
+++ files/phone/benchmark_50.sh
@@ -3,16 +3,26 @@ trace_dir=/sys/kernel/debug/tracing
 sync
 echo 3 > /proc/sys/vm/drop_caches
 
+# **** ALSO:  comment in/out "ondemand" at bottom ****
 stop mpdecision
 echo "1" > /sys/devices/system/cpu/cpu2/online
 echo "1" > /sys/devices/system/cpu/cpu3/online
 echo "1" > /sys/devices/system/cpu/cpu0/online
 echo "1" > /sys/devices/system/cpu/cpu1/online
-echo "userspace" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "2265600" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
-echo "2265600" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_setspeed
-echo "2265600" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_setspeed 
-echo "2265600" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_setspeed 
+echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "performance" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "performance" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "performance" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+#echo "userspace" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+#echo "2265600" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
+#echo "2265600" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_setspeed
+#echo "2265600" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_setspeed 
+#echo "2265600" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_setspeed 
+
+#echo 0 > $trace_dir/events/phonelab_syscall_tracing/enable
+#echo 0 > $trace_dir/events/sched/plsc_exec/enable
+#echo 0 > $trace_dir/events/sched/plsc_fork/enable
+setenforce 0
 
 echo 150000 > $trace_dir/buffer_size_kb
 #echo 300000 > $trace_dir/buffer_size_kb
@@ -40,3 +50,9 @@ echo 0 > $trace_dir/events/sched/sched_switch/enable
 echo 0 > $trace_dir/events/block/block_rq_insert/enable
 echo 0 > $trace_dir/events/block/block_rq_complete/enable
 echo 0 > $trace_dir/events/cpufreq_interactive/enable
+
+echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
