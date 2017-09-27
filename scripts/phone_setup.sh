f9f5b81f675d361f50d1c36ece563ebbb0735b28
carlnues@buffalo.edu <carlnues@buffalo.edu>
2017-09-27 19:35:59 -0400

    Changed setup scripts on both client and phone from hardcoded to parameterized configuration of cache size

--- scripts/phone_setup.sh
+++ scripts/phone_setup.sh
@@ -1,8 +1,13 @@
-# setup phone, default cache
+# setup phone
 
-printf "Pushing files for device %s\n" $1
+printf "Pushing files for device %s for DB cache $2\n" $1
  
 adb -s $1 root
 adb -s $1 push files/phone/benchmark.sh /data/benchmark.sh
 adb -s $1 push files/phone/removeBenchmarkData.sh /data/removeBenchmarkData.sh
 adb -s $1 push files/phone/preBenchmark.sh /data/preBenchmark.sh
+
+if [ -n "$2" ]; then
+	adb -s $1 push files/bdb/100MB/DB_CONFIG /data/
+fi
+
