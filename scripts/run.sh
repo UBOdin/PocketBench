5ef885b7f391564dd122ff92ea856eadd99cc701
carlnues@buffalo.edu <carlnues@buffalo.edu>
2017-09-26 21:37:20 -0400

    Removed hardcoding of workload / database from client-side run script -- now 1 file.  Also added more output info and removed unnecessary delay.

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,7 @@ printf "Rebooting and running benchmark on device %s\n" $1
 adb -s $1 reboot
 sleep 70s
 adb -s $1 root
-sleep 2m
+sleep 1m
 adb -s $1 shell sh /data/removeBenchmarkData.sh
 adb -s $1 shell sh /data/preBenchmark.sh #create database 
 adb -s $1 shell pm disable com.example.benchmark_withjson
@@ -18,13 +18,15 @@ adb -s $1 shell sh /data/benchmark.sh #run queries
 
 # pull log
 adb -s $1 pull /data/trace.log
-mv trace.log logs/YCSB_WorkloadIB_TimingAbdb100.log
-gzip logs/YCSB_WorkloadIB_TimingAbdb100.log
+# $2 = workload (A, B, C etc.); $3 = DB (sql, bdb, bdb100)
+filename="YCSB_Workload${3}TimingA${2}.log"
+mv trace.log logs/$filename
+gzip logs/$filename
 sleep 1
+printf "FILENAME:  %s\n" "$filename"
 
-adb -s $1 shell pm disable com.example.benchmark_withjson
-sleep 1
-adb -s $1 shell pm enable com.example.benchmark_withjson
+printf "RESULTS:"
+adb shell cat /data/results.txt
 
 printf "Completed benchmark for device %s\n" $1
 
