57d5a3939690dda14f0bc4adba766295fad26dab
carlnues@buffalo.edu <carlnues@buffalo.edu>
2017-10-03 12:35:11 -0400

    On-phone script:  Changed governor selection from hardcoded to parameterized.  Also added support for interactive governor.  Client script:  Changed delay during phone reboot / root from hardcoded to block / test.  Now calls phone script with governor parameter.

--- files/phone/benchmark.sh
+++ files/phone/benchmark.sh
@@ -3,16 +3,26 @@ trace_dir=/sys/kernel/debug/tracing
 sync
 echo 3 > /proc/sys/vm/drop_caches
 
-# **** ALSO:  comment in/out "ondemand" at bottom ****
+governor=$1
+# Sanity check the governor choice ("performance" or "interactive")
+if [ "$governor" != "performance" ] && [ "$governor" != "interactive" ]; then
+	echo "Invalid governor" > /data/results.txt
+	exit 1
+fi
+# Stop mpdecision to permit setting the desired governor on all 4 cores:
 stop mpdecision
 echo "1" > /sys/devices/system/cpu/cpu2/online
 echo "1" > /sys/devices/system/cpu/cpu3/online
 echo "1" > /sys/devices/system/cpu/cpu0/online
 echo "1" > /sys/devices/system/cpu/cpu1/online
-echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "performance" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo "performance" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo "performance" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+echo "$governor" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "$governor" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "$governor" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "$governor" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+# Restart mpdecision if interactive (turned-off cores retain governor specified)
+if [ "$governor" = "interactive" ]; then
+	start mpdecision
+fi
 
 #echo 0 > $trace_dir/events/phonelab_syscall_tracing/enable
 #echo 0 > $trace_dir/events/sched/plsc_exec/enable
@@ -42,6 +52,7 @@ am start -n com.example.benchmark_withjson/com.example.benchmark_withjson.MainAc
 
 # Block until app completes run and outputs exit info:
 cat /data/results.pipe > /data/results.txt
+echo "Governor used:  $governor" >> /data/results.txt
 
 echo 0 > $trace_dir/tracing_on
 cat $trace_dir/trace > /data/trace.log
@@ -51,8 +62,11 @@ echo 0 > $trace_dir/events/block/block_rq_insert/enable
 echo 0 > $trace_dir/events/block/block_rq_complete/enable
 echo 0 > $trace_dir/events/cpufreq_interactive/enable
 
-echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+# Scale back CPU if needed to prevent burnout
+if [ "$governor" = "performance" ]; then
+	echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+	echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+	echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+fi
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,20 +6,34 @@ adb -s $1 shell pm enable com.example.benchmark_withjson
 printf "Rebooting and running benchmark on device %s\n" $1
 
 adb -s $1 reboot
-sleep 70s
-adb -s $1 root
-sleep 1m
+adb -s $1 wait-for-device
+sleep 15
+printf "Rebooted\n"
+adb root
+sub=""
+while [ "$sub" != "uid=0(" ]; do
+	sleep 5
+	var=$(adb shell id)
+	sub=${var%%root*}  # var:4:1
+	printf ".\n"
+done
+sleep 15
+printf "Rooted\n"
+
+governor="performance"
+#governor="interactive"
+
 adb -s $1 shell sh /data/removeBenchmarkData.sh
 adb -s $1 shell sh /data/preBenchmark.sh #create database 
 adb -s $1 shell pm disable com.example.benchmark_withjson
 sleep 5
 adb -s $1 shell pm enable com.example.benchmark_withjson
-adb -s $1 shell sh /data/benchmark.sh #run queries
+adb -s $1 shell sh /data/benchmark.sh $governor #run queries
 
 # pull log
 adb -s $1 pull /data/trace.log
 # $2 = workload (A, B, C etc.); $3 = DB (sql, bdb, bdb100)
-filename="YCSB_Workload${3}TimingA${2}.log"
+filename="YCSB_Workload${3}_TimingA${2}.log"
 mv trace.log logs/$filename
 gzip logs/$filename
 sleep 1
