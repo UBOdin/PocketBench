bf0fcdf3747353c6d9086882db3d9c863e603859
carlnues <carlnues@buffalo.edu>
2018-07-19 14:44:19 -0400

    Removed copyover of timing files prebuild. Rather, timings are already dynamic.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,4 @@
 
-# Copy over timing files
-
-rm -rf apps/Benchmark_withJson/app/src/main/res/raw
-cp -r files/json/raw apps/Benchmark_withJson/app/src/main/res/
-
 # build and install
 
 cd apps/Benchmark_withJson/
