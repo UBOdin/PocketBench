affde683c89b6533c1c142cc7832cb0f146d4f7d
carlnues <carlnues@buffalo.edu>
2018-08-17 15:25:55 -0400

    Refactored governor control code in on-phone script

--- files/phone/benchmark.sh
+++ files/phone/benchmark.sh
@@ -4,6 +4,8 @@ sync
 echo 3 > /proc/sys/vm/drop_caches
 
 governor=$1
+default="interactive"
+cpus="0 1 2 3" # List of cpus on phone
 
 # Sanity check the governor choice to supported values:
 if [ "$governor" = "performance" ]; then
@@ -25,14 +27,11 @@ fi
 
 # Stop mpdecision to permit setting the desired governor on all 4 cores:
 stop mpdecision
-echo "1" > /sys/devices/system/cpu/cpu2/online
-echo "1" > /sys/devices/system/cpu/cpu3/online
-echo "1" > /sys/devices/system/cpu/cpu0/online
-echo "1" > /sys/devices/system/cpu/cpu1/online
-echo "$governor" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "$governor" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo "$governor" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo "$governor" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+# Turn on all CPUs and set governor as selected:
+for i in $cpus; do
+	echo "1" > /sys/devices/system/cpu/cpu$i/online
+	echo "$governor" > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+done
 # Restart mpdecision for all governors (turned-off cores retain governor specified)
 start mpdecision
 
@@ -56,9 +55,9 @@ echo 1 > $trace_dir/tracing_on
 
 # Set up IPC pipe to retrieve end-of-run info from app:
 rm /data/results.pipe
-#mknod /data/results.pipe p
-#chmod 777 /data/results.pipe
-/data/makepipe /data/results.pipe # mknod is MIA; chmod is weird and tries to read the pipe
+mknod /data/results.pipe p
+chmod 777 /data/results.pipe
+#/data/makepipe /data/results.pipe # mknod is MIA; chmod is weird and tries to read the pipe
 
 #am kill-all
 am start -n com.example.benchmark_withjson/com.example.benchmark_withjson.MainActivity
@@ -80,14 +79,11 @@ echo 0 > $trace_dir/events/cpufreq_interactive/enable
 
 # Reset governor to default (ondemand) (need to reactivate all cores first):
 stop mpdecision
-echo "1" > /sys/devices/system/cpu/cpu2/online
-echo "1" > /sys/devices/system/cpu/cpu3/online
-echo "1" > /sys/devices/system/cpu/cpu0/online
-echo "1" > /sys/devices/system/cpu/cpu1/online
-echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+# Turn on all CPUs and reset governor to back to default:
+for i in $cpus; do
+	echo "1" > /sys/devices/system/cpu/cpu$i/online
+	echo $default > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+done
 start mpdecision
 
 
