5f8d03443fe1d105290bc417e1ee9b1ee2603b06
carlnues@buffalo.edu <carlnues@buffalo.edu>
2019-04-30 20:25:43 -0400

    Added processing scripts for benchmark and energy tracefiles

---
+++ process/calculate_null.py
@@ -0,0 +1,156 @@
+
+import json
+import sys
+import gzip
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import math
+import statistics
+
+
+def mean_margin(data_list):
+
+	n = 0
+	mean = 0.0
+	stdev = 0.0
+	zstar = 1.645 # for 90% -- 1.96 # for 95%
+	margin = 0.0
+
+	n = len(data_list)
+	mean = statistics.mean(data_list)
+	stdev = statistics.stdev(data_list) # stdev() or pstdev()
+	margin = zstar * (stdev / math.sqrt(n))
+
+	return mean, margin
+
+#end_def
+
+
+def get_energy(input_filename, end_line):
+
+	filename = ""
+	input_file = "" # file obj
+	input_line = ""
+	iteration = -1
+	input_list = []
+	amps = 0.0
+	volts = 0.0
+	watts = 0.0
+	amps_total = 0.0
+	watts_total = 0.0
+	start_line = 0
+
+	input_file = gzip.open(input_filename, "r")
+	input_file.readline() # Skip drop count
+
+	start_line = 5000 * 10
+
+	while (True):
+
+		iteration += 1
+
+		#input_line = str(input_file.readline())[2:-1]
+		input_line = input_file.readline().decode("ascii")
+
+		if (input_line == ""):
+			# Overran logfile.  Return 0:
+			amps_total = 0
+			watts_total = 0
+			break
+		#end_if
+
+		if (iteration < start_line):
+			continue
+		#end_if
+
+		#'''
+		if (iteration == end_line):
+			break
+		#end_if
+		#'''
+
+		input_list = input_line.split("\t")
+
+		amps = float(input_list[1])
+		volts = float(input_list[2])
+
+		watts = amps * volts
+		amps_total += amps
+		watts_total += watts
+
+	#end_while
+
+	input_file.close()
+
+	amps_total = amps_total * 1000.0 / (5000.0 * 3600.0)
+	watts_total = watts_total * 1000.0 / (5000.0 * 3600.0)
+
+	'''
+	print("Current:  " + str(amps_total))
+	print("Energy:  " + str(watts_total))
+	'''
+
+	return amps_total
+
+#end_def
+
+
+def get_mean_error(end_line):
+
+	pathname = ""
+	energy_prefix = "monsoon_NULL_N_null_"
+	energy = 0.0
+	energy_list = []
+	energy_mean = 0.0
+	energy_error = 0.0
+	energy_mean_list = []
+
+	workload_list = ["userspace_300000", "userspace_1267200", "userspace_2649600", "interactive_none", "ondemand_none"]
+	directory_list = ["null_01", "null_02", "null_03"]
+
+	for e in workload_list:
+
+		energy_list = []
+		for f in directory_list:
+			pathname = f + "/" + energy_prefix + e + ".gz"
+			energy = get_energy(pathname, end_line)
+			print(pathname + " " + str(energy))
+			energy_list.append(energy)
+		#end_for
+		print(energy_list)
+		energy_mean, energy_error = mean_margin(energy_list)
+		print(energy_mean, energy_error)
+		energy_mean_list.append(energy_mean)
+
+	#end_for
+
+	return energy_mean_list
+
+#end_def
+
+
+def main():
+
+	print("Hello World")
+
+	nulltimes_dict = {}
+
+	for i in range(500000, 1001000, 50000):
+
+		print("STARTING:  " + str(i))
+		end_line = i
+		nulltimes_dict[end_line] = get_mean_error(end_line)
+
+	#end_for
+
+	print(nulltimes_dict)
+
+#end_def
+
+
+main()
+
+
---
+++ process/process_tracefiles.py
@@ -0,0 +1,234 @@
+
+import json
+import sys
+import gzip
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import math
+import statistics
+
+
+nulltimes_dict = {500000: [1564.7242294777395, 1595.1765554428189, 1669.7928495702693, 1620.5320003296044, 1715.461473335889], 550000: [1600.4479020167478, 1627.64847990463, 1704.3507143738918, 1653.7938166646395, 1749.2191938004794], 600000: [1639.9539755138485, 1667.4842010715363, 1749.0088937080448, 1692.08775879662, 1790.4702978713299], 650000: [1671.8970167900595, 1716.8904899293607, 1798.9363764917587, 1742.1218750642067, 1841.4443802031392], 700000: [1725.4139562390337, 0.0, 0.0, 1779.0527080523582, 1876.4501131665938], 750000: [1760.8833322213964, 0.0, 0.0, 0.0, 0.0], 800000: [1797.0874112901806, 0.0, 0.0, 0.0, 0.0], 850000: [1823.7450706706704, 0.0, 0.0, 0.0, 0.0], 900000: [1846.1918920324952, 0.0, 0.0, 0.0, 0.0], 950000: [1860.5999930220562, 0.0, 0.0, 0.0, 0.0], 1000000: [0.0, 0.0, 0.0, 0.0, 0.0]}
+
+
+def mean_margin(data_list):
+
+	n = 0
+	mean = 0.0
+	stdev = 0.0
+	zstar = 1.645 # for 90% -- 1.96 # for 95%
+	margin = 0.0
+
+	n = len(data_list)
+	mean = statistics.mean(data_list)
+	stdev = statistics.stdev(data_list) # stdev() or pstdev()
+	margin = zstar * (stdev / math.sqrt(n))
+
+	return mean, margin
+
+#end_def
+
+
+def get_latency(input_filename):
+
+	input_file = "" # File object
+	logline = ""
+	iteration = -1
+	starttime = 0.0
+	endtime = 0.0
+	latency = 0.0
+
+	input_file = gzip.open(input_filename)
+
+	while (True):
+
+		iteration += 1
+		if (iteration % 1000 == 0):
+			#break
+			#print("Iteration:  ", iteration)
+			pass
+		#end_if
+
+		#logline = str(input_file.readline())[2:-1]
+		logline = input_file.readline().decode("ascii")
+
+		if (logline == ""):
+			break
+		#end_if
+
+		if ("_START" in logline):
+			starttime += float(logline[33:46])
+		#end_if
+
+		if ("_END" in logline):
+			endtime += float(logline[33:46])
+		#end_if
+
+	#end_while
+
+	input_file.close()
+
+	latency = endtime - starttime
+
+	'''
+	print("iterations:  %d" % (iteration))
+	print(starttime)
+	print(endtime)
+	print("Latency:  ", latency)
+	'''
+
+	return latency
+
+#end_def
+
+
+def get_energy(input_filename, workload):
+
+	filename = ""
+	input_file = "" # file obj
+	input_line = ""
+	iteration = -1
+	input_list = []
+	amps = 0.0
+	volts = 0.0
+	watts = 0.0
+	amps_total = 0.0
+	watts_total = 0.0
+	null_total = 0.0
+	amps_net = 0.0
+	start_line = 0
+	end_line = 0
+
+	input_file = gzip.open(input_filename, "r")
+	input_file.readline() # Skip drop count
+
+	start_line = 5000 * 10
+	end_line = start_line + 5000
+
+	while (True):
+
+		iteration += 1
+
+		#input_line = str(input_file.readline())[2:-1]
+		input_line = input_file.readline().decode("ascii")
+
+		if (input_line == ""):
+			break
+		#end_if
+
+		if (iteration < start_line):
+			continue
+		#end_if
+
+		'''
+		if (iteration == end_line):
+			break
+		#end_if
+		'''
+
+		input_list = input_line.split("\t")
+
+		amps = float(input_list[1])
+		volts = float(input_list[2])
+
+		watts = amps * volts
+		amps_total += amps
+		watts_total += watts
+
+	#end_while
+
+	input_file.close()
+
+	amps_total = amps_total * 1000.0 / (5000.0 * 3600.0)
+	watts_total = watts_total * 1000.0 / (5000.0 * 3600.0)
+
+	null_total = (nulltimes_dict[iteration])[workload]
+
+	#'''
+	print("Current:  " + str(amps_total))
+	print("Energy:  " + str(watts_total))
+
+	print("Null:  " + str(null_total))
+
+	print("Net:  " + str(amps_total - null_total))
+
+	#'''
+
+	return amps_total
+
+#end_def
+
+
+def get_mean_error():
+
+	latency_prefix = "YCSB_SQL_A_0ms_"
+	path = ""
+	pathname = ""
+	latency = 0.0
+	latency_list = []
+	latency_mean = 0.0
+	latency_error = 0.0
+
+	energy_prefix = "monsoon_SQL_A_0ms_"
+	energy = 0.0
+	energy_list = []
+	energy_mean = 0.0
+	energy_error = 0.0
+
+	workload_list = ["userspace_300000", "userspace_1267200", "userspace_2649600", "interactive_none", "ondemand_none"]
+
+	directory_list = ["monsoon_01", "monsoon_02", "monsoon_03"]
+
+	for i in range(len(workload_list)):
+
+		'''
+		latency_list = []
+		for f in directory_list:
+			pathname = f + "/" + latency_prefix + workload_list[i] + ".gz"
+			latency = get_latency(pathname)
+			print(pathname + " " + str(latency))
+			latency_list.append(latency)
+		#end_for
+		print(latency_list)
+		latency_mean, latency_error = mean_margin(latency_list)
+		print(latency_mean, latency_error)
+		'''
+
+		#'''
+		energy_list = []
+		for f in directory_list:
+			pathname = f + "/" + energy_prefix + workload_list[i] + ".gz"
+
+			energy = get_energy(pathname, i)
+
+			print(pathname + " " + str(energy))
+
+			energy_list.append(energy)
+
+		#end_for
+
+		print(energy_list)
+
+		energy_mean, energy_error = mean_margin(energy_list)
+		#'''
+
+	#end_for
+
+#end_def
+
+
+def main():
+
+	print("Hello World")
+
+	get_mean_error()
+
+#end_def
+
+
+main()
+
+
