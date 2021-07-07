35d38c8ba737a1235df6fbb45d9c3d78d2829927
carlnues@buffalo.edu <carlnues@buffalo.edu>
2021-07-07 15:54:40 -0400

    microbenchmarks 1-2:  added perf support for collecting cpu cycles

--- files/phone/experiment_1.c
+++ files/phone/experiment_1.c
@@ -3,6 +3,7 @@
 #include <asm/unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/perf_event.h>
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
@@ -10,7 +11,8 @@
 #include <unistd.h>
 
 #define OUTPUT_SIZE 256
-#define PI 3.14159265
+#define PERFBUFF_SIZE 64
+#define OUTPUT_SIZE 256
 
 #define PRINTLOG(...) output_len = snprintf( output_buff, OUTPUT_SIZE, __VA_ARGS__ ); \
 	result = write(trace_fd, output_buff, output_len); \
@@ -39,10 +41,39 @@ int main(int argc, char** argv) {
 	char trace_filename[] = "/sys/kernel/debug/tracing/trace_marker";
 	char output_buff[OUTPUT_SIZE];
 	int output_len;
+	struct perf_event_attr pea_struct;
+	int perf_cycles_fd;
+	char perf_buff[PERFBUFF_SIZE];
+	unsigned long cycles;
 
 	long long loopcount;
 	long long innercount;
 	long long sum;
+
+	// Initialize HW performance monitoring structure:
+	memset(&pea_struct, 0, sizeof(pea_struct));
+	pea_struct.type = PERF_TYPE_HARDWARE;
+	pea_struct.size = sizeof(struct perf_event_attr);
+	pea_struct.config = PERF_COUNT_HW_CPU_CYCLES;
+	pea_struct.sample_period = 0;  // Not using sample periods; will do manual collection
+	pea_struct.sample_type = 0;  // ditto above
+	pea_struct.read_format = PERF_FORMAT_GROUP;  // | PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
+	// Some bitfields:
+	pea_struct.disabled = 1;  // Disabled for now -- will start collection later
+	pea_struct.inherit = 0;  // No -- main thread only (n.b. documentation says this = 1 is incompatible with PERF_FORMAT_GROUP -- it is _not_; bug?)
+	pea_struct.pinned = 0;  // N.b. pinned = 1 _is_ incompatible with PERF_FORMAT_GROUP -- either a bug in kernel or documentation?
+	pea_struct.exclusive = 0;
+	pea_struct.exclude_user = 0;  // Track userspace
+	pea_struct.exclude_kernel = 1;  // And kernel
+	pea_struct.exclude_hv = 1;  // NOT HV (if any)
+
+	// Open perf fd:
+	result = syscall(__NR_perf_event_open, &pea_struct, 0, -1, -1, 0);
+	if (result == -1) {
+		errlog();
+		return -2;
+	}
+	perf_cycles_fd = result;
 
 //printf("Experiment 1\n");
 	// Open handle to ftrace to save output:
@@ -69,6 +100,10 @@ int main(int argc, char** argv) {
 		_Exit(1);
 	}
 
+	// Enable collection:
+	ioctl(perf_cycles_fd, PERF_EVENT_IOC_RESET, 0);
+	ioctl(perf_cycles_fd, PERF_EVENT_IOC_ENABLE, 0);
+
 	sum = 0;
 	innercount = 20;
 	for (long long i = 0; i < loopcount; i++) {
@@ -77,9 +112,31 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	// Disable collection:
+	ioctl(perf_cycles_fd, PERF_EVENT_IOC_DISABLE, 0);
+
 //printf("loopcount:  %llu  sum:  %llu\n", loopcount, sum);
 	PRINTLOG("SQL_experiment_1:  loopcount:  %lld  sum:  %lld\n", loopcount, sum);
 	PRINTLOG("SQL_END");
+
+	// Collect results:
+	result = read(perf_cycles_fd, perf_buff, PERFBUFF_SIZE);
+	if (result == -1) {
+		errlog();
+		return -3;
+	}
+
+	// Sanity (should be collecting 1 item):
+	if (((unsigned long*)perf_buff)[0] != 1) {
+		__android_log_print(ANDROID_LOG_VERBOSE, "PocketData", "Error:  Bad perf itemcount\n");
+		return -5;
+	}
+
+	cycles = ((unsigned long*)perf_buff)[1];
+	PRINTLOG("Cycle data:  %lu", cycles);
+
+	close(perf_cycles_fd);
+	close(trace_fd);
 
 	return 0;
 
--- files/phone/experiment_2.c
+++ files/phone/experiment_2.c
@@ -3,6 +3,7 @@
 #include <asm/unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/perf_event.h>
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,7 +12,7 @@
 #include <unistd.h>
 
 #define OUTPUT_SIZE 256
-#define PI 3.14159265
+#define PERFBUFF_SIZE 64
 
 #define PRINTLOG(...) output_len = snprintf( output_buff, OUTPUT_SIZE, __VA_ARGS__ ); \
 	result = write(trace_fd, output_buff, output_len); \
@@ -40,6 +41,10 @@ int main(int argc, char** argv) {
 	char trace_filename[] = "/sys/kernel/debug/tracing/trace_marker";
 	char output_buff[OUTPUT_SIZE];
 	int output_len;
+	struct perf_event_attr pea_struct;
+	int perf_cycles_fd;
+	char perf_buff[PERFBUFF_SIZE];
+	unsigned long cycles;
 
 	long long loopcount;
 	long long batchcount;
@@ -48,6 +53,31 @@ int main(int argc, char** argv) {
 	long sleep_us;
 
 	struct timespec interval;
+
+	// Initialize HW performance monitoring structure:
+	memset(&pea_struct, 0, sizeof(pea_struct));
+	pea_struct.type = PERF_TYPE_HARDWARE;
+	pea_struct.size = sizeof(struct perf_event_attr);
+	pea_struct.config = PERF_COUNT_HW_CPU_CYCLES;
+	pea_struct.sample_period = 0;  // Not using sample periods; will do manual collection
+	pea_struct.sample_type = 0;  // ditto above
+	pea_struct.read_format = PERF_FORMAT_GROUP;  // | PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
+	// Some bitfields:
+	pea_struct.disabled = 1;  // Disabled for now -- will start collection later
+	pea_struct.inherit = 0;  // No -- main thread only (n.b. documentation says this = 1 is incompatible with PERF_FORMAT_GROUP -- it is _not_; bug?)
+	pea_struct.pinned = 0;  // N.b. pinned = 1 _is_ incompatible with PERF_FORMAT_GROUP -- either a bug in kernel or documentation?
+	pea_struct.exclusive = 0;
+	pea_struct.exclude_user = 0;  // Track userspace
+	pea_struct.exclude_kernel = 1;  // And kernel
+	pea_struct.exclude_hv = 1;  // NOT HV (if any)
+
+	// Open perf fd:
+	result = syscall(__NR_perf_event_open, &pea_struct, 0, -1, -1, 0);
+	if (result == -1) {
+		errlog();
+		return -2;
+	}
+	perf_cycles_fd = result;
 
 //printf("Experiment 2\n");
 	// Open handle to ftrace to save output:
@@ -77,6 +107,10 @@ int main(int argc, char** argv) {
 	batchcount = 50000;
 	sleep_us = 100;
 
+	// Enable collection:
+	ioctl(perf_cycles_fd, PERF_EVENT_IOC_RESET, 0);
+	ioctl(perf_cycles_fd, PERF_EVENT_IOC_ENABLE, 0);
+
 	sum = 0;
 	innercount = 20;
 	for (long long i = 0; i < batchcount; i++) {
@@ -94,9 +128,31 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	// Disable collection:
+	ioctl(perf_cycles_fd, PERF_EVENT_IOC_DISABLE, 0);
+
 //printf("Degrees:  %f\n", degree);
 	PRINTLOG("SQL_experiment_2:  loopcount:  %lld  batchcount:  %lld  sleep_us:  %ld  sum:  %lld", loopcount, batchcount, sleep_us, sum);
 	PRINTLOG("SQL_END");
+
+	// Collect results:
+	result = read(perf_cycles_fd, perf_buff, PERFBUFF_SIZE);
+	if (result == -1) {
+		errlog();
+		return -3;
+	}
+
+	// Sanity (should be collecting 1 item):
+	if (((unsigned long*)perf_buff)[0] != 1) {
+		__android_log_print(ANDROID_LOG_VERBOSE, "PocketData", "Error:  Bad perf itemcount\n");
+		return -5;
+	}
+
+	cycles = ((unsigned long*)perf_buff)[1];
+	PRINTLOG("Cycle data:  %lu", cycles);
+
+	close(perf_cycles_fd);
+	close(trace_fd);
 
 	return 0;
 
