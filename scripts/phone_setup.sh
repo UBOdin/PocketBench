54efa394802e855e198a46268e9c1f336bcf1d5f
carlnues@buffalo.edu <carlnues@buffalo.edu>
2022-05-16 14:45:24 -0400

    phone pre-script:  remove device parameterization

--- scripts/phone_setup.sh
+++ scripts/phone_setup.sh
@@ -1,13 +1,9 @@
 # setup phone
 
-printf "Pushing files for device %s for DB cache $2\n" $1
+printf "Pushing files for device\n"
  
-adb -s $1 root
-adb -s $1 push files/phone/benchmark.sh /data/benchmark.sh
-adb -s $1 push files/phone/preBenchmark.sh /data/preBenchmark.sh
-adb -s $1 push files/phone/start_benchmark.sh /data/start_benchmark.sh
-
-if [ -n "$2" ]; then
-	adb -s $1 push files/bdb/100MB/DB_CONFIG /data/
-fi
+adb root
+adb push files/phone/benchmark.sh /data/benchmark.sh
+adb push files/phone/preBenchmark.sh /data/preBenchmark.sh
+adb push files/phone/start_benchmark.sh /data/start_benchmark.sh
 
