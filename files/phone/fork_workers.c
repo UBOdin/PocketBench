e3fbca579c77a17ac4d3b4d66e493c4c0cad5bb9
carlnues@buffalo.edu <carlnues@buffalo.edu>
2022-10-02 18:39:24 -0400

    new file:  parent code to fork worker procs

---
+++ files/phone/fork_workers.c
@@ -0,0 +1,77 @@
+
+#include <android/log.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define THREADCOUNT 8 
+
+
+#define errtrap(error) (__errtrap(result, error, __LINE__))
+void __errtrap(int result, const char* error, int line) {
+
+	if (result == -1) {
+//		printf("Error in %s() on line %d:  %s\n", error, line, strerror(errno));
+		__android_log_print(ANDROID_LOG_VERBOSE, "FORK_WORKERS", "Error:  %d %s\n", errno, strerror(errno));
+		_exit(errno);
+	}
+	return;
+
+}
+
+
+void errlog() {
+
+	__android_log_print(ANDROID_LOG_VERBOSE, "PocketData", "Error:  %d %s\n", errno, strerror(errno));
+//printf("Error:  %d %s\n", errno, strerror(errno));
+
+	return;
+
+}
+
+
+int main(int argc, char** argv) {
+
+	int result;
+	char binpath[] = "/data/compute.exe";
+	int pidpool[THREADCOUNT];
+	int wstatus;
+
+	for (int i = 0; i < THREADCOUNT; i++) {
+		result = fork();
+		errtrap("fork");
+		if (result == 0) {
+			result = execv(binpath, argv);
+			errtrap("execv");
+			_exit(99);  // Never gets here
+		}
+		if (result > 0) {
+			pidpool[i] = result;
+		}
+//printf("Forked thread %d\n", result);
+	}
+
+	for (int i = 0; i < THREADCOUNT; i++) {
+		result = waitpid(pidpool[i], &wstatus, 0);
+		errtrap("waitpid");
+
+		if (WIFEXITED(wstatus) == 0) {
+			__android_log_print(ANDROID_LOG_VERBOSE, "FORK_WORKERS", "Error:  child thread %d terminated\n", result);
+			_exit(111);
+		}
+
+		if (WEXITSTATUS(wstatus) != 0) {
+			__android_log_print(ANDROID_LOG_VERBOSE, "FORK_WORKERS", "Error:  child thread %d retval %d\n", result, WEXITSTATUS(wstatus));
+//printf("Child pid %d error retval:  %d\n", pidpool[i], WEXITSTATUS(wstatus));
+			_exit(122);
+		}
+//printf("Thread %d retval %d\n", pidpool[i], WEXITSTATUS(wstatus));
+
+	}
+
+//printf("Clean exit\n");
+	_exit(0);
+
+}
