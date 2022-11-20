cef9d9d8d21b971ab1bf6b1828c1a611b04495ba
carlnues@buffalo.edu <carlnues@buffalo.edu>
2022-11-20 18:37:20 -0500

    new shell script:  runs in background to oscillate CPU speed

---
+++ files/phone/oscillate.sh
@@ -0,0 +1,27 @@
+
+cpu_dir=/sys/devices/system/cpu
+
+freq_little_lo=$1
+freq_little_hi=$3
+freq_big_lo=$2
+freq_big_hi=$4
+
+interval=".02"
+
+trace_log=/sys/kernel/debug/tracing/trace_marker
+echo "little lo:  $freq_little_lo  big lo:  $freq_big_lo  little hi:  $freq_little_hi  big hi:  $freq_big_hi" >> $trace_log
+
+while [ "1" = "1" ]; do
+
+	sleep $interval
+
+	echo "$freq_little_hi" > $cpu_dir/cpufreq/policy0/scaling_setspeed
+	echo "$freq_big_hi" > $cpu_dir/cpufreq/policy4/scaling_setspeed
+	
+	sleep $interval
+
+	echo "$freq_little_lo" > $cpu_dir/cpufreq/policy0/scaling_setspeed
+	echo "$freq_big_lo" > $cpu_dir/cpufreq/policy4/scaling_setspeed
+
+done
+			
