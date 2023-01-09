766df089cc9656191f166ffdb82b1542076da46c
carlnues@buffalo.edu <carlnues@buffalo.edu>
2023-01-09 12:21:12 -0500

    AUIA:  collecting per-CPU cycle counts

--- apps/Test/app/src/main/cpp/perflib.cpp
+++ apps/Test/app/src/main/cpp/perflib.cpp
@@ -13,15 +13,26 @@
 
 #define PERFBUFF_SIZE 64
 #define OUTPUT_SIZE 256
+#define CPU_MAX 8
+
+#define PRINTLOG(...) output_len = snprintf( output_buff, OUTPUT_SIZE, __VA_ARGS__ ); \
+	retval = write(trace_fd, output_buff, output_len); \
+	errtrap("write"); \
+	__android_log_print(ANDROID_LOG_VERBOSE, "MACROBENCH", "Log:  %s\n", output_buff);
+	
+
+static int trace_fd;
+static int cpu_arr[CPU_MAX];
 
 
-static int perf_cycles_fd = 0;
+#define errtrap(error) (__errtrap(retval, error, __LINE__))
+void __errtrap(int retval, const char* error, int line) {
 
-
-static void errlog() {
-
-	__android_log_print(ANDROID_LOG_VERBOSE, "PocketData", "Error:  %d %s\n", errno, strerror(errno));
-
+	if (retval == -1) {
+//		printf("Error in %s() on line %d:  %s\n", error, line, strerror(errno));
+		__android_log_print(ANDROID_LOG_VERBOSE, "MACROBENCH", "Error:  %d %s\n", errno, strerror(errno));
+		_exit(errno);
+	}
 	return;
 
 }
@@ -29,37 +40,49 @@ static void errlog() {
 
 extern "C" JNIEXPORT jint JNICALL Java_com_example_test_MetaTest_startcount(JNIEnv *env, jclass clazz, jint param) {
 
-	int result;
+	int retval;
 	struct perf_event_attr pea_struct;
+	int pid;
+	char trace_filename[] = "/sys/kernel/debug/tracing/trace_marker";
+	char output_buff[OUTPUT_SIZE];
+	int output_len;
 
-	// Initialize HW performance monitoring structure:
-	memset(&pea_struct, 0, sizeof(pea_struct));
-	pea_struct.type = PERF_TYPE_HARDWARE;
-	pea_struct.size = sizeof(struct perf_event_attr);
-	pea_struct.config = PERF_COUNT_HW_CPU_CYCLES;
-	pea_struct.sample_period = 0;  // Not using sample periods; will do manual collection
-	pea_struct.sample_type = 0;  // ditto above
-	pea_struct.read_format = PERF_FORMAT_GROUP;  // | PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
-	// Some bitfields:
-	pea_struct.disabled = 1;  // Disabled for now -- will start collection later
-	pea_struct.inherit = 0;  // No -- main thread only (n.b. documentation says this = 1 is incompatible with PERF_FORMAT_GROUP -- it is _not_; bug?)
-	pea_struct.pinned = 0;  // N.b. pinned = 1 _is_ incompatible with PERF_FORMAT_GROUP -- either a bug in kernel or documentation?
-	pea_struct.exclusive = 0;
-	pea_struct.exclude_user = 0;  // Track userspace
-	pea_struct.exclude_kernel = 1;  // And kernel
-	pea_struct.exclude_hv = 1;  // NOT HV (if any)
+	// Open handle to ftrace to save output:
+	retval = open(trace_filename, O_WRONLY);
+	errtrap("open");
+	trace_fd = retval;
+	PRINTLOG("Starting perf collection %d", param);
 
-	// Open perf fd:
-	result = syscall(__NR_perf_event_open, &pea_struct, 0, -1, -1, 0);
-	if (result == -1) {
-		errlog();
-		return -2;
+	// Open array of perf fds, for each CPU:
+	for (int i = 0; i < CPU_MAX; i++) {
+
+		// Initialize HW performance monitoring structure:
+		memset(&pea_struct, 0, sizeof(pea_struct));
+		pea_struct.type = PERF_TYPE_HARDWARE;
+		pea_struct.size = sizeof(struct perf_event_attr);
+		pea_struct.config = PERF_COUNT_HW_CPU_CYCLES;
+		pea_struct.sample_period = 0;  // Not using sample periods; will do manual collection
+		pea_struct.sample_type = 0;  // ditto above
+		pea_struct.read_format = PERF_FORMAT_GROUP;  // | PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
+		// Some bitfields:
+		pea_struct.disabled = 1;  // Disabled for now -- will start collection later
+		pea_struct.inherit = 0;  // No -- main thread only (n.b. documentation says this = 1 is incompatible with PERF_FORMAT_GROUP -- it is _not_; bug?)
+		pea_struct.pinned = 0;  // N.b. pinned = 1 _is_ incompatible with PERF_FORMAT_GROUP -- either a bug in kernel or documentation?
+		pea_struct.exclusive = 0;
+		pea_struct.exclude_user = 0;  // Track userspace
+		pea_struct.exclude_kernel = 0;  // And kernel
+		pea_struct.exclude_hv = 1;  // NOT HV (if any)
+
+		// Get perf fd to track all tasks (-1) on a given CPU (i):
+		pid = -1;
+		retval = syscall(__NR_perf_event_open, &pea_struct, pid, i, -1, 0);
+		errtrap("syscall");
+		cpu_arr[i] = retval;
+		// Enable collection:
+		ioctl(retval, PERF_EVENT_IOC_RESET, 0);
+		ioctl(retval, PERF_EVENT_IOC_ENABLE, 0);
+
 	}
-	perf_cycles_fd = result;
-
-	// Enable collection:
-	ioctl(perf_cycles_fd, PERF_EVENT_IOC_RESET, 0);
-	ioctl(perf_cycles_fd, PERF_EVENT_IOC_ENABLE, 0);
 
 	return 0;
 
@@ -68,58 +91,42 @@ extern "C" JNIEXPORT jint JNICALL Java_com_example_test_MetaTest_startcount(JNIE
 
 extern "C" JNIEXPORT jint JNICALL Java_com_example_test_MetaTest_stopcount(JNIEnv *env, jclass clazz, jint param) {
 
-	int result;
+	int retval;
 	char perf_buff[PERFBUFF_SIZE];
-	int trace_fd;
-	char trace_filename[] = "/sys/kernel/debug/tracing/trace_marker";
 	char output_buff[OUTPUT_SIZE];
 	int output_len;
 	unsigned long cycles;
+	int perf_fd;
+	unsigned long cycles_arr[CPU_MAX];
 
-	// Errtrap uninitialized fd:
-	if (perf_cycles_fd <= 0) {
-		return -1;
+	for (int i = 0; i < CPU_MAX; i++) {
+
+		perf_fd = cpu_arr[i];
+		// Errtrap uninitialized fd:
+		if (perf_fd <= 2) {
+			return -2;
+		}
+		// Collect results:
+		retval = read(perf_fd, perf_buff, PERFBUFF_SIZE);
+		errtrap("read");
+		// Disable collection:
+//		ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
+		// Sanity (should be collecting 1 item):
+		if (((unsigned long*)perf_buff)[0] != 1) {
+			return -3;
+		}
+		cycles = ((unsigned long*)perf_buff)[1];
+//		PRINTLOG("Cycle data for CPU %d:  %lu", i, cycles);
+		// Cleanup:
+//		close(perf_fd);
+		cycles_arr[i] = cycles;
+
 	}
 
-	// Collect results:
-	result = read(perf_cycles_fd, perf_buff, PERFBUFF_SIZE);
-	if (result == -1) {
-		errlog();
-		return -3;
-	}
+	PRINTLOG("Cycle data #%d:  %lu %lu %lu %lu %lu %lu %lu %lu", param, cycles_arr[0], cycles_arr[1], cycles_arr[2], cycles_arr[3], cycles_arr[4], cycles_arr[5], cycles_arr[6], cycles_arr[7]);
 
-	// Disable collection:
-	ioctl(perf_cycles_fd, PERF_EVENT_IOC_DISABLE, 0);
-
-	// Sanity (should be collecting 1 item):
-	if (((unsigned long*)perf_buff)[0] != 1) {
-		__android_log_print(ANDROID_LOG_VERBOSE, "PocketData", "Error:  Bad perf itemcount\n");
-		return -5;
-	}
-
-	// Open handle to ftrace to save output:
-	result = open(trace_filename, O_WRONLY);
-	if (result == -1) {
-		errlog();
-		return -4;
-	}
-	trace_fd = result;
-
-	memset(&output_buff, 0, sizeof(output_buff));
-	cycles = ((unsigned long*)perf_buff)[1];
-	output_len = snprintf(output_buff, OUTPUT_SIZE, "Cycle data:  %lu", cycles);
-
-	result = write(trace_fd, output_buff, output_len);
-	if (result == -1) {
-		errlog();
-		return -6;
-	}
-
-	// Cleanup:
-	close(perf_cycles_fd);
-	close(trace_fd);
-	perf_cycles_fd = 0;  // signal tracing is off
-
+//	PRINTLOG("End perf collection %d", param);
+//	close(trace_fd);
 	return 0;
 
 }
