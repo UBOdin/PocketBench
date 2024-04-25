6f131cc05b875ca19652355dae10d9cd0726d50f
carlnues@buffalo.edu <carlnues@buffalo.edu>
2024-04-24 23:24:12 -0400

    fork binary:  distribute 1 full workload to each cpu in cpu mask (ignore previous worker threadcount parameter)

--- files/phone/fork_workers.c
+++ files/phone/fork_workers.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -35,29 +36,15 @@ int main(int argc, char** argv) {
 	int pidpool[THREADMAX];
 	int wstatus;
 	int trace_fd;
-	char trace_filename[] = "/sys/kernel/debug/tracing/trace_marker";
+//	char trace_filename[] = "/sys/kernel/debug/tracing/trace_marker";
+	char trace_filename[] = "/sys/kernel/tracing/trace_marker";
 	char output_buff[OUTPUT_SIZE];
 	int output_len;
-	int distribute_big;
-	int distribute_little;
-	long long loopcount;
+	int bit;
+	int bitmask;
 
 	memset(&output_buff, 0, sizeof(output_buff));
-
-	threadcount = atoi(argv[1]);
-	if (threadcount > THREADMAX) {
-			__android_log_print(ANDROID_LOG_VERBOSE, "FORK_WORKERS", "Error:  workers > MAX\n");
-			_exit(133);
-	}
-
-	distribute_big = 0;
-	if ((argv[3][0] == 'c') || (argv[3][0] == 'e') || (argv[3][0] == 'f')) {
-		distribute_big = 1;
-	}
-	distribute_little = 0;
-	if ((argv[3][1] == 'c') || (argv[3][1] == 'e') || (argv[3][1] == 'f')) {
-		distribute_little = 1;
-	}
+	memset(&pidpool, 0, sizeof(pidpool));
 
 	// Open handle to ftrace to save output:
 	result = open(trace_filename, O_WRONLY);
@@ -65,50 +52,39 @@ int main(int argc, char** argv) {
 	trace_fd = result;
 	PRINTLOG("FORK_START");
 
+	threadcount = 8;
+	bit = 1;
+	bitmask = strtoul(argv[3], NULL, 16);
+
 	for (int i = 0; i < threadcount; i++) {
-
-		char newval;
-		if (i == 0) {
-			newval = '8';
+		snprintf(argv[3], 3, "%02x", bit);
+		if ((bitmask & bit) != 0) {
+			result = fork();
+			errtrap("fork");
+			if (result == 0) {
+				result = execv((const char*)argv[2], argv + 2);
+				errtrap("execv");
+				_exit(99);  // Never gets here
+			}
+			if (result > 0) {
+				pidpool[i] = result;
+			}
 		}
-		if (i == 1) {
-			newval = '4';
-		}
-		if (i == 2) {
-			newval = '2';
-		}
-		if (i == 3) {
-			newval = '1';
-		}
-		if (distribute_big == 1) {
-			argv[3][0] = newval;
-		}
-		if (distribute_little == 1) {
-			argv[3][1] = newval;
-		}
-
-		result = fork();
-		errtrap("fork");
-		if (result == 0) {
-			result = execv((const char*)argv[2], argv + 2);
-			errtrap("execv");
-			_exit(99);  // Never gets here
-		}
-		if (result > 0) {
-			pidpool[i] = result;
-		}
+		bit = bit << 1;
 	}
 
 	for (int i = 0; i < threadcount; i++) {
-		result = waitpid(pidpool[i], &wstatus, 0);
-		errtrap("waitpid");
-		if (WIFEXITED(wstatus) == 0) {
-			__android_log_print(ANDROID_LOG_VERBOSE, "Microbench Parent", "Error:  pid %d terminated\n", result);
-			_exit(111);
-		}
-		if (WEXITSTATUS(wstatus) != 0) {
-			__android_log_print(ANDROID_LOG_VERBOSE, "Microbench Parent", "Error:  pid %d retval %d\n", result, WEXITSTATUS(wstatus));
-			_exit(122);
+		if (pidpool[i] != 0) {
+			result = waitpid(pidpool[i], &wstatus, 0);
+			errtrap("waitpid");
+			if (WIFEXITED(wstatus) == 0) {
+				__android_log_print(ANDROID_LOG_VERBOSE, "Microbench Parent", "Error:  pid %d terminated\n", result);
+				_exit(111);
+			}
+			if (WEXITSTATUS(wstatus) != 0) {
+				__android_log_print(ANDROID_LOG_VERBOSE, "Microbench Parent", "Error:  pid %d retval %d\n", result, WEXITSTATUS(wstatus));
+				_exit(122);
+			}
 		}
 	}
 
